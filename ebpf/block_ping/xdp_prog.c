1d7413cfd7cb8ecd12b5adfeadeb5f90d5888c07
Guy Davidi <guydav1111@gmail.com>
2025-01-18 19:39:05 +0200

    add block ping src
    Signed-off-by: Guy Davidi <guydav1111@gmail.com>

--- ebpf/block_ping/xdp_prog.c
+++ ebpf/block_ping/xdp_prog.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <signal.h>
-#include <linux/bpf.h>
-#include <bpf/bpf.h>
-#include <bpf/libbpf.h>
+#include <errno.h>
 #include <arpa/inet.h>
 #include <net/if.h>
+#include <linux/bpf.h>
+#include <linux/if_link.h>
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
 #include "xdp_prog.h"
 #include "xdp_prog.skel.h"
 
-// Global variables for cleanup
+// Global variables for cleanup and detachment.
 static struct xdp_prog *skel = NULL;
 static struct ring_buffer *ringbuf = NULL;
-static struct bpf_link *link = NULL;
+static unsigned int g_ifindex = 0;
+static int g_xdp_flags = 0;
 
 // Signal handler for cleanup
 void handle_sigint(int sig) {
     printf("\nTerminating using signal %d ...\n", sig);
 
+    // Detach the XDP program if attached.
+    if (g_ifindex) {
+        if (bpf_xdp_detach(g_ifindex, g_xdp_flags, NULL) < 0) {
+            perror("bpf_xdp_detach");
+        }
+        g_ifindex = 0;
+    }
+
     if (ringbuf) {
         ring_buffer__free(ringbuf);
         ringbuf = NULL;
-    }
-
-    if (link) {
-        bpf_link__destroy(link);
-        link = NULL;
     }
 
     if (skel) {
@@ -54,10 +61,20 @@ int handle_event(void *ctx __attribute__((unused)), void *data, size_t len __att
 
 int main(int argc, char *argv[]) {
     unsigned int ifindex;
+    // Default flag: allow update on an interface with no XDP program
+    int xdp_flags = XDP_FLAGS_UPDATE_IF_NOEXIST;
+    int use_skb_mode = 0;
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <interface>\n", argv[0]);
+    if (argc < 2 || argc > 3) {
+        fprintf(stderr, "Usage: %s <interface> [--skb]\n", argv[0]);
         return 1;
+    }
+
+    if (argc == 3 && strcmp(argv[2], "--skb") == 0) {
+        xdp_flags |= XDP_FLAGS_SKB_MODE;
+        use_skb_mode = 1;
+    } else {
+        xdp_flags |= XDP_FLAGS_DRV_MODE; // Use native driver mode by default
     }
 
     ifindex = if_nametoindex(argv[1]);
@@ -65,6 +82,9 @@ int main(int argc, char *argv[]) {
         perror("if_nametoindex");
         return 1;
     }
+    // Save for later detachment.
+    g_ifindex = ifindex;
+    g_xdp_flags = xdp_flags;
 
     // Set up signal handler
     signal(SIGINT, handle_sigint);
@@ -76,32 +96,42 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Attach the XDP program to the specified interface
-    link = bpf_program__attach_xdp(skel->progs.forward_non_ping, ifindex);
-    if (!link) {
-        fprintf(stderr, "Failed to attach XDP program to interface %s (ifindex %u)\n", argv[1], ifindex);
+    // Retrieve the file descriptor of the BPF program we want to attach.
+    int prog_fd = bpf_program__fd(skel->progs.forward_non_ping);
+    if (prog_fd < 0) {
+        fprintf(stderr, "Failed to get BPF program file descriptor\n");
         xdp_prog__destroy(skel);
         return 1;
     }
 
-    // Set up the ring buffer
+    // Attach the XDP program with the given flags
+    int err = bpf_xdp_attach(ifindex, prog_fd, xdp_flags, NULL);
+    if (err < 0) {
+        fprintf(stderr, "Failed to attach XDP program to interface %s (ifindex %u) in %s mode: %s\n",
+                argv[1], ifindex, use_skb_mode ? "SKB" : "native", strerror(-err));
+        xdp_prog__destroy(skel);
+        return 1;
+    }
+
+    // Set up the ring buffer for events.
     ringbuf = ring_buffer__new(bpf_map__fd(skel->maps.ringbuf), handle_event, NULL, NULL);
     if (!ringbuf) {
         fprintf(stderr, "Failed to create ring buffer\n");
         handle_sigint(0);
     }
 
-    printf("Successfully started! Press Ctrl+C to stop.\n");
+    printf("Successfully started in %s mode! Press Ctrl+C to stop.\n", use_skb_mode ? "SKB" : "native");
 
-    // Poll the ring buffer
+    // Poll the ring buffer.
     while (1) {
-        if (ring_buffer__poll(ringbuf, 1000 /* timeout, ms */) < 0) {
-            fprintf(stderr, "Error polling ring buffer\n");
+        err = ring_buffer__poll(ringbuf, 1 /* timeout, ms */);
+        if (err < 0) {
+            fprintf(stderr, "Error polling ring buffer: %d\n", err);
             break;
         }
     }
 
-    // Cleanup (if loop exits)
+    // Cleanup if loop exits.
     handle_sigint(0);
     return 0;
 }
